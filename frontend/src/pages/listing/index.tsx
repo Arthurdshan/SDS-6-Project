46c5bf0bb700a45a2fe9595c333149dd541cac7b
Arthur <arthurhan269@gmail.com>
2022-01-14 17:11:30 -0300

    use state, use effect

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,7 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import MovieCard from "../../components/movieCard";
 import Pagination from "../../components/pagination";
+import { BASE_URL } from "../../utils/requests";
 
 const Listing = () => {
+
+    const [pageNumber, setPageNumber] = useState(0);
+    
+    const getData = async () => {
+        try {
+            const { data } = await axios.get(`${BASE_URL}/movies`);
+            console.log(data);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    useEffect(() => {
+        (async () => {
+            await getData();
+        })();
+    }, []);
+
     return (
         <>
             <Pagination />

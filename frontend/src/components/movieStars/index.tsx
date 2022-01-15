0d6e14fc797897c3c9a6116edf4f76dade3c680b
Arthur <arthurhan269@gmail.com>
2022-01-15 01:04:00 -0300

    show score

--- frontend/src/components/movieStars/index.tsx
+++ frontend/src/components/movieStars/index.tsx
@@ -3,14 +3,47 @@ import { ReactComponent as StarHalf } from '../../assets/img/star-half.svg';
 import { ReactComponent as StarEmpty } from '../../assets/img/star-empty.svg';
 import './styles.css';
 
-const MovieStars = () => {
+type Props = {
+    score: number;
+}
+
+type StarProps = {
+    fill: number;
+}
+
+const getFills = (score: number) => {
+
+    const fills = [0, 0, 0, 0, 0];
+
+    const integerPart = Math.floor(score);
+
+    for (let i = 0; i < integerPart; i++) {
+        fills[i] = 1;
+    }
+
+    const diff = score - integerPart;
+    if (diff > 0) {
+        fills[integerPart] = 0.5;
+    }
+
+    return fills;
+}
+
+const Star = ({ fill } : StarProps) => {
+    return fill === 0 ? <StarEmpty /> : fill === 1 ? <StarFull /> : <StarHalf />
+}
+
+const MovieStars = ({ score } : Props) => {
+
+    const fills = getFills(score);
+
     return (
         <div className="dsmovie-stars-container">
-            <StarFull />
-            <StarFull />
-            <StarFull />
-            <StarHalf />
-            <StarEmpty />
+            <Star fill={fills[0]}/>
+            <Star fill={fills[1]}/>
+            <Star fill={fills[2]}/>
+            <Star fill={fills[3]}/>
+            <Star fill={fills[4]}/>
         </div>
     );
 }
